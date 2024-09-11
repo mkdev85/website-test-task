f6c253883ede1c52b3dca225f42ad5abdf3a04a5
Mayank <mkdev8585@gmail.com>
2024-09-11 16:51:12 +0530

    Fixed responsive design for website list

--- frontend/src/components/WebsitesPage/Header/Header.styles.tsx
+++ frontend/src/components/WebsitesPage/Header/Header.styles.tsx
@@ -1,6 +1,7 @@
 import { AppBar, styled } from '@mui/material';
 
 export const HeaderWrapper = styled(AppBar)(({ theme }) => ({
+  padding: theme.spacing(0, 2),
   '& .MuiToolbar-root': {
     maxWidth: theme.spacing(80),
     marginLeft: 'auto',
@@ -8,6 +9,7 @@ export const HeaderWrapper = styled(AppBar)(({ theme }) => ({
     width: '100%',
     display: 'flex',
     gap: theme.spacing(1),
+    padding: 0,
   },
 
   '& .header-text': {
